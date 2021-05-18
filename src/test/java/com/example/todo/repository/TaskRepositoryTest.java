daff6e2af8accd5ed22b2ae779e8a8a197fbf2f1
robinmartinsson <martinsson.r@gmail.com>
2021-05-18 09:27:13 +0200

    added test for TaskRepository

---
+++ src/test/java/com/example/todo/repository/TaskRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.example.todo.repository;
+
+import com.example.todo.models.Task;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataMongoTest
+class TaskRepositoryTest {
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    @Test
+    void findTaskByActive() {
+
+        Task mockTask = new Task();
+        mockTask.setDescription("grocery shopping");
+        mockTask.setActive(true);
+        taskRepository.save(mockTask);
+
+        List<Task> taskResult = taskRepository.findTaskByActive(true);
+        String actual = taskResult.get(0).getDescription();
+        String expected = "grocery shopping";
+        String notExpected = "take a shower";
+
+        assertEquals(expected, actual);
+        assertNotEquals(notExpected, actual);
+    }
+
+    @Test
+    void findTaskByNotActive() {
+        Task mockTask = new Task();
+        mockTask.setDescription("grocery shopping");
+        mockTask.setActive(false);
+        taskRepository.save(mockTask);
+
+        List<Task> taskResult = taskRepository.findTaskByActive(false);
+        String actual = taskResult.get(0).getDescription();
+        String expected = "grocery shopping";
+        String notExpected = "take a shower";
+
+        assertEquals(expected, actual);
+        assertNotEquals(notExpected, actual);
+    }
+}
